86b7fee24e9b4e8ab56c283c67a2d799611b8dae
Alexrusskikh <105988086+Alexrusskikh@users.noreply.github.com>
2022-08-22 12:20:28 +0500

    Update borndayforewer.py

--- borndayforewer.py
+++ borndayforewer.py
@@ -1,16 +1,22 @@
+'''в моей  версии Paython словарь упорядочен, - проводил  испытания, и  в словарь можно  добавть еще вопросы...'''
+literature_dict = {'год рождения А.С.Пушкина': '1799', 'дату рождения А.С Пушкина': '6 июня'}
+mathematics_dict = {'2x2': '4', '5x5': '25'}
+#функция принимает словари вопросов по предметам, перебирает пары и  задает  вопросы
+print("Внимаение - тестирование началось!")
+def long_separator(count):
+    return'*' * count
 
-def  test ( year='1799', day='6 июня'):
-    BornYearPushkin = input("Укажите год рождения А.С Пушкина: ")
-    while BornYearPushkin != "1799":
-        BornYearPushkin = input("Уточните год в справочнике и введите вновь...")
-    print("Верно!!!!")
+def  test (subject):
+    for key in subject: #итератор по ключам словаря
+        answer = input("Укажите ответ - " + key + " \n")
+        if answer == subject[key]:
+            print("Верно!!!")
+        else:
+            print("Не верно...")
 
-    BornDaуPushkin = input("Укажите дату рождения А.С Пушкина, например - '6 июня': ")
-    while BornDaуPushkin != '6 июня':
-        BornDaуPushkin = input("Уточните год в справочнике и введите вновь...")
-    print("Верно!!!!")
-
-    print("Тест  закончен")
-
-
-test()
+print(long_separator(30))
+test(literature_dict)
+print(long_separator(30))
+test(mathematics_dict)
+print(long_separator(30))
+print("Тест  закончен")
